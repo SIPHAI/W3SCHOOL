677344f735d9f86cb518aac3ee08731c6abeaadc
Siphai PHOL <100112002+SIPHAI@users.noreply.github.com>
2022-07-23 00:00:26 +0700

    Add files via upload

---
+++ Ex06  Generate a list and tuple with comma-separated numbers.py
@@ -0,0 +1,6 @@
+values = input("Input some comma seprated numbers : ")
+list = values.split(",")   #To get the somma for all each number
+tuple = tuple(list) #make the list for 
+print('List : ',list)
+print('Tuple : ',tuple)
+
