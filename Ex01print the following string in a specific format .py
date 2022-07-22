e3fcf76d606fc56fb08f45a8c67fb39b29bec773
Siphai PHOL <100112002+SIPHAI@users.noreply.github.com>
2022-07-22 23:57:47 +0700

    Add files via upload

---
+++ Ex01print the following string in a specific format .py
@@ -0,0 +1,2 @@
+print("Twinkle, twinklelittle star,\n\tHow I wonder what you are! \n\t   Up above the world so high,\n\t     Like a diamond in the sky")
+print("Twinkle, twinkle, little star,\n\t    How I wonder what you are")
