cb1d6133b7f341ae0124817244e112cb3c21d12d
Siphai PHOL <100112002+SIPHAI@users.noreply.github.com>
2022-07-23 23:06:42 +0700

    Add files via upload

---
+++ Ex08display the first and last colors from the following list.py
@@ -0,0 +1,8 @@
+# Write a Python program to display the first and last colors from the following list.
+color_list = ["Red","Green","White" ,"Black"]
+print("The first color is :", color_list[+1])
+print("The last color is:", color_list[-1])
+
+# other way in correction
+# color_list = ["Red","Green","White" ,"Black"]
+# print( "%s %s"%(color_list[0],color_list[-1]))
